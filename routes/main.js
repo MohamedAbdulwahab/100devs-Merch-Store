8a8aaaea079d81b7013c9ca07519f663e9a7884b
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2023-01-06 13:12:44 -0800

    added server, database, routes, models, and controls setup

--- routes/main.js
+++ routes/main.js
@@ -2,12 +2,10 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
-const postsController = require("../controllers/posts");
 const { ensureAuth } = require("../middleware/auth");
 
 //Main Routes 
 router.get("/", homeController.getIndex);
-router.get("/profile", ensureAuth, postsController.getProfile);
 
 //Routes for user login/signup
 router.get("/login", authController.getLogin);
--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const flash = require("express-flash");
 const logger = require("morgan");
 const connectDB = require("./config/database");
 const mainRoutes = require("./routes/main");
-const postRoutes = require("./routes/posts");
 
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
@@ -55,7 +54,6 @@ app.use(flash());
 
 //Setup Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
-app.use("/post", postRoutes);
 
 
 //Server Running
